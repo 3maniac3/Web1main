319b57bb9efc5e77a8fb9b2211014fd70a8f5961
GeoCosmic <157273849+3maniac3@users.noreply.github.com>
2024-11-02 18:12:26 +0700

    Update copyright.js

--- js/copyright.js
+++ js/copyright.js
@@ -3,5 +3,5 @@ const copyrightYear = document.getElementById("copyright");
 window.addEventListener("load", () => {
     const date = new Date;
     
-    copyrightYear.textContent = `©${date.getFullYear()} Berkilau`;
+    copyrightYear.textContent = `© ${date.getFullYear()} Berkilau`;
 });
