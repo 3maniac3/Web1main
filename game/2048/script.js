247477fff35f235903513bc94a9432729cddded4
GeoCosmic <157273849+3maniac3@users.noreply.github.com>
2024-11-06 13:03:24 +0700

    Add files via upload

--- game/2048/script.js
+++ game/2048/script.js
@@ -1,16 +1,32 @@
 const info = document.getElementById("info");
 const score = document.getElementById("score");
 const highScore = document.getElementById("high-score");
+const gameBone = document.querySelector(".game-bone");
+const gameLayer = document.querySelector(".game-layer");
 
 const radius = 50;
 let startX, startY, moveX, moveY;
 let point = 0, highPoint = 0;
 
-window.addEventListener("load", () => { getScore(); generate(2); });
+window.addEventListener("load", () => { 
+    innerSub(); getScore(); generate(2); repaint(); 
+});
+
 window.addEventListener("touchstart", touch);
 window.addEventListener("touchmove", swipe);
 window.addEventListener("touchend", move);
 window.addEventListener("keypress", keyPressed);
+
+function innerSub() {
+    for (let i = 0; i < 16; i++) {
+        const bone = document.createElement("div");
+        const layer = document.createElement("div");
+        bone.classList.add("bone");
+        layer.classList.add("layer");
+        gameBone.appendChild(bone);
+        gameLayer.appendChild(layer);
+    }
+}
 
 function getScore() {
     highPoint = localStorage.getItem("highScore");
@@ -22,19 +38,20 @@ function generate(times) {
     
     for (let i = 0; i < times; i++) {
         if (boxes.length == 0) break;
+        let persentage = 1;
         const index = Math.floor(Math.random() * (boxes.length - 0) + 0);
-        boxes[index].textContent = 2;
+        if (times == 1) persentage = Math.floor(Math.random() * (11 - 1) + 1);
+        boxes[index].textContent = persentage < 10 ? 2 : 4;
+        boxes[index].style.animation = "newBox .3s linear";
         boxes.splice(index, 1);
     }
-    
-    repaint();
 }
     
 function repaint() {
     [...document.querySelectorAll(".box")].forEach(x => {
         const number = Number(x.textContent);
         x.classList.remove("lock");
-        
+        x.style.boxShadow = "none";
         if (number == 2) {
             x.style.color = "#000";
             x.style.backgroundColor = "#efe7dc";
@@ -54,18 +71,23 @@ function repaint() {
             x.style.color = "#fff";
             x.style.backgroundColor = "#f74b1b";
         } else if (number == 128) {
+            x.style.boxShadow = "0 0 10px #fcd988";
             x.style.color = "#fff";
             x.style.backgroundColor = "#fcd988";
         } else if (number == 256) {
+            x.style.boxShadow = "0 0 10px #f7dd7e";
             x.style.color = "#fff";
             x.style.backgroundColor = "#f7dd7e";
         } else if (number == 512) {
+            x.style.boxShadow = "0 0 20px #f2c659";
             x.style.color = "#fff";
             x.style.backgroundColor = "#f2c659";
         } else if (number == 1024) {
+            x.style.boxShadow = "0 0 20px #fac332";
             x.style.color = "#fff";
             x.style.backgroundColor = "#fac332";
         } else if (number == 2048) {
+            x.style.boxShadow = "0 0 30px #feb316";
             x.style.color = "#fff";
             x.style.backgroundColor = "#feb316";
         } else if (number > 2048) {
@@ -80,17 +102,18 @@ function repaint() {
 function touch(e) {
     startX = e.touches[0].clientX;
     startY = e.touches[0].clientY;
+    [...document.querySelectorAll(".box")].forEach(x => x.style.animation = "none");
 }
 
 function swipe(e) {
     if (info.classList.contains("beggining")) info.style.display = "none";
+    else if (info.classList.contains("win")) info.style.display = "none";
     moveX = e.touches[0].clientX;
     moveY = e.touches[0].clientY;
 }
 
 function move(e) {
     const boxes = document.querySelectorAll(".box")
-    
     if (moveX > startX + radius) operate(boxes, [2, 6, 10, 14], 3, 1, "right");
     else if (moveX < startX - radius) operate(boxes, [1, 5, 9, 13], 3, 1, "left");
     else if (moveY > startY + radius) operate(boxes, [8, 9, 10, 11], 12, 4, "down");
@@ -99,8 +122,7 @@ function move(e) {
 
 function keyPressed(e) {
     const boxes = document.querySelectorAll(".box")
-    if (info.style.display != "none") info.style.display = "none";
-    
+    if (info.classList.contains("beggining")) info.style.display = "none";
     if (e.key == "d") operate(boxes, [2, 6, 10, 14], 3, 1, "right");
     else if (e.key == "a") operate(boxes, [1, 5, 9, 13], 3, 1, "left");
     else if (e.key == "s") operate(boxes, [8, 9, 10, 11], 12, 4, "down");
@@ -108,8 +130,9 @@ function keyPressed(e) {
 }
 
 function operate(box, index, range, step, move) {
+    const layers = document.querySelectorAll(".layer");
+    console.log(layers);
     let next = false;
-    
     for (let i of index) {
         for (let t = 0; t < 3; t++) {
             for (let j = 0; j < range; j += step) {
@@ -145,16 +168,14 @@ function operate(box, index, range, step, move) {
             }
         }
     }
-    
-    repaint();
     if (next) generate(1);
+    repaint();
     scoreOperate();
     finalCheck();
 }
 
 function scoreOperate() {
     score.textContent = point;
-    
     if (highPoint < point){
         highPoint = point;
         highScore.textContent = highPoint;
@@ -163,7 +184,6 @@ function scoreOperate() {
 
 function finalCheck() {
     const boxes = document.querySelectorAll(".box");
-    
     for (let x of boxes) {
         if (Number(x.textContent) >= 2048) {
             info.textContent = "Victorious!";
@@ -172,7 +192,6 @@ function finalCheck() {
             return;
         }
     }
-    
     for (let x of boxes) {
         if (x.textContent == "") return;
     } for (let i = 0; i < boxes.length; i++) {
@@ -182,9 +201,7 @@ function finalCheck() {
         if (i == 12 || i == 13 || i == 14 || i == 15) continue;
         if (boxes[i].textContent == boxes[i+4].textContent) return;
     }
-    
     if (point > localStorage.getItem("highScore")) localStorage.setItem("highScore", point);
-    
     info.textContent = "Game Over!";
     info.setAttribute("class", "lose");
     info.style.display = "block";
@@ -195,11 +212,11 @@ function reset() {
         x.textContent = "";
         x.style.backgroundColor = "#ccc0b3";
     });
-    
     info.textContent = "Swipe to start";
     info.setAttribute("class", "beggining");
     info.style.display = "block";
     point = 0;
     score.textContent = point;
     generate(2);
+    repaint();
 }
